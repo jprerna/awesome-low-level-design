a174d66913ab8985a812771435b683ff1fe2e5e2
Prerna Jain <jprerna@users.noreply.github.com>
2025-05-13 17:34:33 +0530

    Added decorator innings.py

--- solutions/python/cricinfo/innings.py
+++ solutions/python/cricinfo/innings.py
@@ -1,21 +1,10 @@
+from dataclasses import dataclass,field
 class Innings:
     def __init__(self, id, batting_team_id, bowling_team_id):
-        self.id = id
-        self.batting_team_id = batting_team_id
-        self.bowling_team_id = bowling_team_id
-        self.overs = []
+        id: int
+        batting_team_id: int
+        bowling_team_id: int
+        overs: list = field(default_factory=list)
 
     def add_over(self, over):
         self.overs.append(over)
-
-    def get_id(self):
-        return self.id
-
-    def get_overs(self):
-        return self.overs
-
-    def get_batting_team_id(self):
-        return self.batting_team_id
-
-    def get_bowling_team_id(self):
-        return self.bowling_team_id
